4ce4a2b02359767865a695066d0de80116d02bc4
Dedt20 <detorresdaina1020@gmail.com>
2025-06-25 12:27:10 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine("   ***     ***   ");
+            Console.WriteLine(" ******   ****** ");
+            Console.WriteLine("******** ********");
+            Console.WriteLine(" *************** ");
+            Console.WriteLine("  *************  ");
+            Console.WriteLine("   ***********   ");
+            Console.WriteLine("    *********    ");
+            Console.WriteLine("     *******     ");
+            Console.WriteLine("      *****      ");
+            Console.WriteLine("       ***       ");
+            Console.WriteLine("        *        ");
+
+            Console.ResetColor();
+
+            Console.WriteLine("\nhi");
+        }
+    }
+}
+
